f52d22699bfe21947ee46810cd63503ae9b662cf
Aydin Elhan <200030355@student.kangan.edu.au>
2023-11-16 13:30:03 +1100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { generate } from "./generate";
+import "./styles/main.scss"
+
+
+console.log(generate())
+console.log(2)
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        // babel for backwards compatibility.
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack app",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+};
